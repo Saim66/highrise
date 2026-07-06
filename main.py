8e647576c7496a6e85b6cca666eaec60218c8bfe
sayem <saimhere2004@gmail.com>
2026-07-06 22:03:10 +0600

    Fix startup command

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
-from highrise import BaseBot, Highrise
 import asyncio
+from highrise import BaseBot, Highrise
 
 class Bot(BaseBot):
     async def on_start(self, session_metadata: dict) -> None:
-        print("Bot is connected!")
+        print("Bot is successfully connected!")
 
 async def main():
     token = os.getenv("BOT_TOKEN")
     room_id = os.getenv("ROOM_ID")
     
+    # Initialize the bot instance
     bot = Bot()
-    # Connect manually
-    await Highrise(room_id, token).run(bot)
+    
+    # Initialize the Highrise connection object
+    hr = Highrise()
+    
+    # Run the bot with the room_id and token passed here
+    await hr.run(room_id, token, bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
